79c4e63916f1b5635fea351e69acadec2d521509
jma7th <jma7th@gmail.com>
2024-09-09 15:29:40 -0300

    mobile fix 3

--- scripts-screen.js
+++ scripts-screen.js
@@ -1,32 +1,13 @@
-function updateLockButton() {
-              const lockButton = document.getElementById("game-fullscreen");
-              const newOrientation = getOppositeOrientation();
-              lockButton.textContent = `Lock to ${newOrientation}`;
-            }
-            
+
             function getOppositeOrientation() {
               return screen
                 .orientation
                 .type
                 .startsWith("portrait") ? "landscape" : "portrait";
-            }
-            
-            async function rotate(lockButton) {
-              if (!document.fullscreenElement) {
-                await document.documentElement.requestFullscreen();
-              }
-              const newOrientation = getOppositeOrientation();
-              await screen.orientation.lock(newOrientation);
-              updateLockButton(lockButton);
-            }
-            
+            }            
             async function rotatescreen(newOrientation) {
               if (!document.fullscreenElement) {
-                await document.documentElement.requestFullscreen();
+                await toggleFullscreen();
               }
               await screen.orientation.lock(newOrientation);
             }
-
-            screen.orientation.addEventListener("change", updateLockButton);
-            
-            window.addEventListener("load", updateLockButton);
